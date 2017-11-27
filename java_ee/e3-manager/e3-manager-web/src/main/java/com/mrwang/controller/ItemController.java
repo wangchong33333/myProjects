e95521bdd1b17e58118b8d4765a5c6b907af69db
mrwang <2384359843@qq.com>
2017-11-28 01:13:23 +0800

    1

--- java_ee/e3-manager/e3-manager-web/src/main/java/com/mrwang/controller/ItemController.java
+++ java_ee/e3-manager/e3-manager-web/src/main/java/com/mrwang/controller/ItemController.java
@@ -4,7 +4,22 @@ import java.io.FileOutputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mrwang.pojo.TbItem;
+import com.mrwang.service.ItemService;
 
 @Controller
 public class ItemController {
+	@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem geItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.geItemById(itemId);
+		return tbItem;
+	}
 }
