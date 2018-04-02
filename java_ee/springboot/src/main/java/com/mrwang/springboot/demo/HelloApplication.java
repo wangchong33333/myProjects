76e5776a3ce4cf68c4f19b717df242eb99eb0241
mrwang <2384359843@qq.com>
2018-04-03 01:03:00 +0800

    1

--- java_ee/springboot/src/main/java/com/mrwang/springboot/demo/HelloApplication.java
+++ java_ee/springboot/src/main/java/com/mrwang/springboot/demo/HelloApplication.java
@@ -2,14 +2,14 @@ package com.mrwang.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@SpringBootApplication
-@Configuration
+@SpringBootApplication(exclude= {RedisAutoConfiguration.class})
 public class HelloApplication {
     
     @RequestMapping("hello")
