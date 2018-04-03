9814591dd0502ba93b7c518d9868caf73cc963e5
mrwang <2384359843@qq.com>
2018-04-03 21:54:29 +0800

    1

--- java_ee/springboot/src/main/java/com/mrwang/springboot/demo/HelloApplication.java
+++ java_ee/springboot/src/main/java/com/mrwang/springboot/demo/HelloApplication.java
@@ -1,25 +1,34 @@
 package com.mrwang.springboot.demo;
 
+import java.nio.charset.Charset;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@SpringBootApplication(exclude= {RedisAutoConfiguration.class})
+@SpringBootApplication(exclude = { RedisAutoConfiguration.class })
 public class HelloApplication {
-    
-    @RequestMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "hello world！";
-    }
-    
-    public static void main(String[] args) {
-        SpringApplication.run(HelloApplication.class, args);
-    }
+
+	@RequestMapping("hello")
+	@ResponseBody
+	public String hello() {
+		return "hello world！雷猴啊";
+	}
+
+//	@Bean
+//	public StringHttpMessageConverter stringHttpMessageConverter() {
+//		StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("ISO-8859-1"));
+//		return converter;
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
 
 }
