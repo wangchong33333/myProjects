2e5f3810fe0cf3891d11b451b6be40a716f79c22
mrwang <2384359843@qq.com>
2018-04-24 09:41:40 +0800

    1

--- front_end/vue_test/src/router/index.js
+++ front_end/vue_test/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import HelloWorld1 from '@/components/HelloWorld1'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/hello1',
+      name: 'HelloWorld1',
+      component: HelloWorld1
     }
   ]
 })
