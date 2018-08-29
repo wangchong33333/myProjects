99ec33ce7eddfea395471dab2b25072b69352a4d
mrwang <2384359843@qq.com>
2018-08-29 14:57:23 +0800

    1

--- java_ee/Spring_Cloud/microservice-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceProviderUserApplication.java
+++ java_ee/Spring_Cloud/microservice-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceProviderUserApplication.java
@@ -2,8 +2,14 @@ package com.itmuch.cloud.microservicesimpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceProviderUserApplication {
 
 	public static void main(String[] args) {
--- java_ee/Spring_Cloud/microservice-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserController.java
+++ java_ee/Spring_Cloud/microservice-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserController.java
@@ -2,18 +2,41 @@ package com.itmuch.cloud.microservicesimpleprovideruser.controller;
 
 import com.itmuch.cloud.microservicesimpleprovideruser.entity.User;
 import com.itmuch.cloud.microservicesimpleprovideruser.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("microservice-provider-user", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/instance-info")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("microservice-provider-user");
+    }
+
     @GetMapping("/simple/{id}")
-    public User findById(@PathVariable Long id){
+    public User findById(@PathVariable Long id) {
         return this.userRepository.getOne(id);
     }
 }
