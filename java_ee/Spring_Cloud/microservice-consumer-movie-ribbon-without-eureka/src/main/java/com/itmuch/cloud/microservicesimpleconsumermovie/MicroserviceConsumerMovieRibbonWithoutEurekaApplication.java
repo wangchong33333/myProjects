f63b24bf37110ec0ab778563a36c60c26e53175f
mrwang <2384359843@qq.com>
2018-08-29 20:11:53 +0800

    1

---
+++ java_ee/Spring_Cloud/microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceConsumerMovieRibbonWithoutEurekaApplication.java
@@ -0,0 +1,21 @@
+package com.itmuch.cloud.microservicesimpleconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MicroserviceConsumerMovieRibbonWithoutEurekaApplication {
+
+    @Bean
+    @LoadBalanced //加了这个才能使用vip
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieRibbonWithoutEurekaApplication.class, args);
+    }
+}
