5bc5fd007d93998a489039fc077625cfd7150670
cwang <a2384359843a@gmail.com>
2018-09-21 00:06:00 +0800

    1

---
+++ java_ee/Spring_Cloud/microservice-gateway-zuul-reg-exp/src/main/java/com/itmuch/cloud/ZuulRegExpApplication.java
@@ -0,0 +1,26 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulRegExpApplication {
+
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper(
+                "(?<name>^.+)-(?<version>v.+$)",
+                "${version}/${name}");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulRegExpApplication.class, args);
+    }
+}
