4ef5bb9fefde8cca7da74b9a37fb3698416cddd5
mrwang <2384359843@qq.com>
2018-10-19 11:28:46 +0800

    1

--- java_ee/Spring_Cloud/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ java_ee/Spring_Cloud/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -3,6 +3,7 @@ package com.itmuch.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Hello world!
