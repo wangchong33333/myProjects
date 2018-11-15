e024f8c9cd9a594c521c79f056ca0612286023c3
mrwang <2384359843@qq.com>
2018-11-15 10:08:05 +0800

    1

---
+++ java_ee/code_test/main/TestStringreplac.java
@@ -0,0 +1,9 @@
+package com.turboes.user.dao;
+
+public class Test {
+    public static void main(String[] args) {
+        String a="15572116700";
+        String replace = a.replace("", "15572116701");
+        System.out.println(replace);
+    }
+}
