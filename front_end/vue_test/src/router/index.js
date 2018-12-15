2c6594002752587b95666e0e25d69a02e17773fd
cwang <a2384359843a@gmail.com>
2018-12-16 02:05:57 +0800

    1

--- front_end/vue_test/src/router/index.js
+++ front_end/vue_test/src/router/index.js
@@ -5,9 +5,14 @@ import HelloWorld1 from '@/components/HelloWorld1'
 
 Vue.use(Router)
 
+function getAbsolutePath () {
+  let path = location.pathname
+  return path.substring(0, path.lastIndexOf('/') + 1)
+}
+
 export default new Router({
   mode: 'history',
-  base: '/vue', // 只在history下有用
+  base: getAbsolutePath(), // 只在history下有用
   routes: [
     {
       path: '/',
