efb03a7a5a78e359e8d71e595372b8c556ffffa6
mrwang <2384359843@qq.com>
2019-01-11 16:14:48 +0800

    1

---
+++ java_ee/Spring_Cloud/microservice-provider-user/src/test/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.itmuch.cloud.microservicesimpleprovideruser.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    @Test
+    public void showInfo() {
+    }
+}
